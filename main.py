8d23bd428ed01f2c8648855eaf6a483e3a4edc11
Georgy <georgy.polkanov@yandex.ru>
2024-01-20 20:28:06 +0300

    setting first message

--- main.py
+++ main.py
@@ -7,12 +7,8 @@ bot = telebot.TeleBot("6970971993:AAGmSbXt-zyhmNZdUsDV2Q3CE02Tw3oAFZ4", parse_mo
 #functions
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-	bot.reply_to(message, "Hello, world?")
+	bot.reply_to(message, "Hello, world")
 
 #buttons
-@bot.message_handler(commands=['button'])
-def button_message(message):
-    markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item1=types.KeyboardButton("Кнопка")
-    markup.add(item1)
-    bot.send_message(message.chat.id,'Выберите что вам надо', reply_markup=markup)
+
+bot.polling(none_stop=True)
